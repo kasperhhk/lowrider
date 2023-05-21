1493d0304d5d43cb2d214c91be409b5ed1605993
kasperhhk <kkr@dis-play.dk>
2023-05-21 18:14:47 +0200

    add basic stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import theme from '../styles/theme'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <meta name="emotion-insertion-point" content="" />
+      </Head>
       <body>
         <Main />
         <NextScript />
